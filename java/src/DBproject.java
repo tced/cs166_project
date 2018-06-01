cba93cc7ac5839c9d6f2b2263f5a7aa579233785
Tiffany Cedeno <tcede001@bolt.cs.ucr.edu>
2018-05-31 23:21:14 -0700

    updated first 4 queries to check for error

--- java/src/DBproject.java
+++ java/src/DBproject.java
@@ -302,61 +302,118 @@ public class DBproject{
 	public static boolean flag = true; 
 	public static void AddPlane(DBproject esql) {//1
 		String query = "INSERT INTO Plane (id, make, model, age, seats) VALUES(";  
-		String plane_ID, plane_make, plane_model, plane_age, plane_seats = ""; 
+		String plane_ID, plane_make, plane_model = "";  
+		int plane_age, plane_seats = 0;
 		try { 
-			//do while loop to find the plane_ID 
+			
+ 			//do while loop to find the plane_ID 
 			do {
 			 	System.out.print("Insert the plane's ID: "); 
 		  		plane_ID = in.readLine();
 				if (plane_ID.length() == 0) {
-				   System.out.print("Invalid ID, please enter again: "); 
+				   System.out.print("Invalid ID, please enter again\n"); 
+				   flag = false; 
+				}
+				else {flag = true; }
+				 	
+			}while(!flag);	
+		
+			//do while loop to find the plane make
+			do {
+				System.out.print("Insert the Plane's make: ");
+		  		plane_make = in.readLine();	
+				if (plane_make.length() == 0 || plane_make.length() > 32) {
+				   System.out.print("Please enter a vaid plane make: "); 
+				   flag = false; 
+				}
+				else{ flag = true; }
+			}while(!flag);
+ 
+			//do while loop to find the plane model 
+		        do {
+				System.out.print("Insert the Plane's model: ");
+		  		plane_model = in.readLine();	
+				if (plane_model.length() == 0 || plane_model.length() > 64) {
+				   System.out.print("Invalid plane model, please enter again\n"); 
+				   flag = false; 
+				}
+				else{ flag = true; }	
+			}while(!flag); 
+	
+			//do while loop to find plane age	
+		        do {
+				System.out.print("Insert the Plane's age: "); 
+		  		plane_age = Integer.parseInt(in.readLine()); 
+				if (plane_age < 0) {
+				   System.out.print("Error plane age cannot be less than 0, please enter again\n"); 
+				   flag = false; 
+				}
+				else{ flag = true; }	
+			}while(!flag); 
+			//do while loop to find plane's seats 
+			do {
+				System.out.print("Insert how many seats the Plane contains: ");
+		  		plane_seats = Integer.parseInt(in.readLine()); 
+				if (plane_seats < 0 || plane_seats > 500) {
+				   System.out.print("Error: you entered a plane seat that's out of bounds. Please Try again.\n"); 
 				   flag = false; 
 				}
 				else{
 				   flag = true; 
 				}
-				/*
-		  		System.out.print("Insert the Plane's make: ");
-		  		String plane_make = in.readLine();
-		  		query += ", \'" + plane_make + "\'"; 
-		  		System.out.print("Insert the Plane's model: ");
-		  		String plane_model = in.readLine(); 
-		  		query += ", \'" + plane_model + "\'";  
-		  		System.out.print("Insert the Plane's age: "); 
-		  		String plane_age = in.readLine(); 
-		  		query += "," + plane_age; 
-		  		System.out.print("Insert how many seats the Plane contains: ");
-		  		String plane_seats = in.readLine(); 
-		 		query += "," + plane_seats + ")\n";
-	
-		 		System.out.print(query);
-		  		esql.executeUpdate(query); 
-			        */ 	
-			}while(!flag);	
-		
-		        query +=  plane_ID;
-			System.out.print(query);  
+			}while(!flag); 
+			query +=  plane_ID + ", \'" + plane_make + "\'" + ", \'" + plane_model + "\'" + "," + plane_age + "," + plane_seats + ")\n";
+			//int rowCount = esql.executeQuery(query); 
+			//System.out.println("total rows(s): " + rowCount); 
 
-			
-  		}
+		 	System.out.print(query);
+		  	esql.executeUpdate(query); 
+			    
+  					}
 		catch (Exception e) {
-			System.out.println("Your input is invalid!");
+			System.err.println(e.getMessage());
+
 		}
 	}
 
 	public static void AddPilot(DBproject esql) {//2
+		String query2 = "INSERT INTO Pilot (id, fullname, nationality) VALUES(";
+		String pilot_ID, pilot_name, pilot_nationality = ""; 
 		try {
-                  String query2 = "INSERT INTO Pilot (id, fullname, nationality) VALUES(";
-                  System.out.print("Insert Pilot's ID: ");
-                  String pilot_ID = in.readLine();
-                  query2 +=  pilot_ID;
-                  System.out.print("Insert Pilot's fullname: ");
-                  String pilot_name = in.readLine();
-                  query2 += ", \'" + pilot_name + "\'";
-                  System.out.print("Insert the Pilot's nationality: ");
-                  String pilot_nationality = in.readLine();
-                  query2 += ", \'" + pilot_nationality + "\');\n";
+                 
+		  //do while loop for pilot id 
+		  do {
+		  	System.out.print("Insert Pilot's ID: ");
+                  	pilot_ID = in.readLine();
+			if (pilot_ID.length() == 0) {
+			   System.out.print("Error: you did not enter a valid pilot ID. Please try again\n"); 
+			   flag = false; 
+			}
+			else {flag = true;}
+                  }while(!flag);
+	         
+		  //do while loop for pilot name 
+                  do {
+			System.out.print("Insert Pilot's fullname: ");
+                  	pilot_name = in.readLine();
+			if (pilot_name.length() > 128) {
+			   System.out.print("Error: invalid pilot name. Please try again\n"); 
+			   flag = false; 
+			}
+			else {flag = true;} 
+                  }while(!flag); 
 
+		  //do while loop for pilot's nationality 
+		  do {
+		   	System.out.print("Insert the Pilot's nationality: ");
+                 	pilot_nationality = in.readLine();
+ 			if (pilot_nationality.length() > 25) {
+			   System.out.print("Error: invalid nationality. Please try again\n"); 
+			}
+			else {flag = true;}
+		  }while(!flag);
+		  query2 +=  pilot_ID + ", \'" + pilot_name + "\'" + ", \'" + pilot_nationality + "\');\n";
+		  
                   System.out.print(query2);
                   esql.executeUpdate(query2);
                 }
@@ -367,41 +424,98 @@ public class DBproject{
 
 	public static void AddFlight(DBproject esql) {//3
 		// Given a pilot, plane and flight, adds a flight in the DB
-		try {
-		  String query3 = "INSERT INTO Flight (fnum, cost, num_sold, num_stops, actual_departure_date, actual_arrival_date, arrival_airport, departure_airport) VALUES("; 
- 		  System.out.print("Insert the flight's fnum: "); 
-		  String flight_fnum = in.readLine();
-		  query3 +=  flight_fnum; 
+		String query3 = "INSERT INTO Flight (fnum, cost, num_sold, num_stops, actual_departure_date, actual_arrival_date, arrival_airport, departure_airport) VALUES("; 
+		String flight_fnum, flight_cost, num_seats_sold, num_flight_stops, plane_dept_date, plane_arrival_date, airport_arrival, airport_depart = "";
 
-		  System.out.print("Insert the flight's cost: ");
-		  String flight_cost = in.readLine();
-		  query3 += ", " + flight_cost; 
-		  
-		  System.out.print("Insert how many seats have been sold: ");
-		  String num_seats_sold = in.readLine(); 
-		  query3 += ", " + num_seats_sold;  
-		  
-		  System.out.print("Insert the number of stops that flight has: "); 
-		  String num_flight_stops = in.readLine(); 
-		  query3 += "," + num_flight_stops; 
-		  
-		  System.out.print("Insert the flight's actual departure date: ");
-		  String plane_dept_date = in.readLine(); 
-		  query3 += ", \'" + plane_dept_date + "\'";
-		  
-		  System.out.print("Insert the flight's actual arrival date: "); 
-		  String plane_arrival_date = in.readLine(); 
-		  query3 += ", \'" + plane_arrival_date + "\'"; 
-		  
-		  System.out.print("Insert the name of airport the flight will be arriving: "); 
-		  String airport_arrival = in.readLine(); 
-		  query3 += ", \'" + airport_arrival + "\'"; 
-		  
-		  System.out.print("Insert the name of airport the flight will be departing from: "); 
-		  String airport_depart = in.readLine(); 
-		  query3 += ", \'" + airport_depart + "\')\n"; 
-			
+		try { 
+		  do { 
+ 		  	System.out.print("Insert the flight's fnum: "); 
+		  	flight_fnum = in.readLine();
+		  	if (flight_fnum.length() == 0) {
+				System.out.print("Error, you did not type a valid flight number. Please try again\n");
+			        flag = false;  
+			}
+ 			else {flag = true;}
+                  }while(!flag); 
+
+		  do {
+		  	System.out.print("Insert the flight's cost: ");
+		  	flight_cost = in.readLine();
+			if (flight_cost.length() == 0) {
+				System.out.print("Error, you did not type a valid flight cost. Please try again\n"); 
+				flag = false; 
+			}
+			else {flag = true;}
+		  }while(!flag); 
+
+		  do{ 
+		  	System.out.print("Insert how many seats have been sold: ");
+		  	num_seats_sold = in.readLine(); 
+		  	if (num_seats_sold.length() == 0) {
+				System.out.print("Error, you did not type a valid number for seats sold. Please try again\n"); 
+				flag = false; 
+			}
+			else {flag = true;}
+
+		  }while(!flag); 
+
+		  do{
+		  	System.out.print("Insert the number of stops that flight has: "); 
+		  	num_flight_stops = in.readLine(); 
+		  	if (num_flight_stops.length() == 0) {
+				System.out.print("Error, you did not type a valid number of flights stop. Please try again\n"); 
+				flag = false; 
+			}
+			else {flag = true;}
+
+		  }while(!flag); 
+
+		  do{
+		  	System.out.print("Insert the flight's actual departure date in the following format YYYY-MM-DD: ");
+		  	plane_dept_date = in.readLine(); 
+		  	if (plane_dept_date.length() == 0) {
+				System.out.print("Error, you did not type a valid departure date. Please try again\n"); 
+				flag = false; 
+			}
+			else {flag = true;}
+
+		  }while(!flag); 
+
+		  do{
+		  	System.out.print("Insert the flight's actual arrival date in the following format YYYY-MM-DD: "); 
+		  	plane_arrival_date = in.readLine(); 
+		  	if (plane_arrival_date.length() == 0) {
+				System.out.print("Error, you did not type a valid flight arrival date. Please try again\n"); 
+				flag = false; 
+			}
+			else {flag = true;}
+
+		  }while(!flag); 
+
+		  do{
+		  	System.out.print("Insert the airport's code that the flight will be arriving at: "); 
+		  	airport_arrival = in.readLine(); 
+		  	if (airport_arrival.length() > 5 || airport_arrival.length() == 0) {
+				System.out.print("Error, you did not type a valid airport name. Please try again\n"); 
+				flag = false; 
+			}
+			else {flag = true;}
+
+		  }while(!flag); 
+
+	          do {
+		  	System.out.print("Insert the airport's code that the flight will be departing from: "); 
+		  	airport_depart = in.readLine(); 
+		  	if (airport_depart.length() == 0 || airport_depart.length() > 5) {
+				System.out.print("Error, you did not type a valid airport code for departure. Please try again\n"); 
+				flag = false; 
+			}
+			else {flag = true;}
+
+		  }while(!flag); 	
 	
+	          query3 +=  flight_fnum + ", " + flight_cost + ", " + num_seats_sold + "," + num_flight_stops + ", \'" + plane_dept_date + "\'" + ", \'" + plane_arrival_date + "\'" + ", \'" + airport_arrival + "\'" + ", \'" + airport_depart + "\')\n"; 
+		  
 		  System.out.print(query3);
 		  esql.executeUpdate(query3); 	
 		}
@@ -412,15 +526,30 @@ public class DBproject{
  	}
 	
 	public static void AddTechnician(DBproject esql) {//4
+                String query4 = "INSERT INTO Technician (id, full_name) VALUES(";
+		String tech_ID, tech_name = ""; 
 		try {
-                  String query4 = "INSERT INTO Technician (id, full_name) VALUES(";
-                  System.out.print("Insert Technician's ID: ");
-                  String tech_ID = in.readLine();
-                  query4 +=  tech_ID;
-                  System.out.print("Insert Technician's fullname: ");
-                  String tech_name = in.readLine();
-                  query4 += ", \'" + tech_name + "\');\n";
-
+		  do{ 
+                  	System.out.print("Insert Technician's ID: ");
+                  	tech_ID = in.readLine();
+                  	if (tech_ID.length() == 0) {
+			  System.out.print("Error, you did not insert a technician's ID. Please Try Again\n"); 
+	  		  flag = false; 
+			}
+                        else {flag = true;} 
+		  }while(!flag); 
+	
+		  do{
+		  	System.out.print("Insert Technician's fullname: ");
+                  	tech_name = in.readLine();
+		        if (tech_name.length() == 0 || tech_name.length() > 128) {
+			  System.out.print("Error, you did not insert a technician's full name. Please try again\n"); 
+			  flag = false; 
+			}
+ 			else {flag = true;} 
+          	  }while(!flag); 
+		
+                  query4 +=  tech_ID + ", \'" + tech_name + "\');\n";
                   System.out.print(query4);
                   esql.executeUpdate(query4);
                 }
